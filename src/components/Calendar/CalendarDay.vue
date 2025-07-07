3a78c861398c4e14422e5f97bdb822d6c6278faf
Marcin Poholski <m.poholski@bokonet.de>
2025-07-07 11:24:26 +0200

    feature: aria disabled

--- src/components/Calendar/CalendarDay.vue
+++ src/components/Calendar/CalendarDay.vue
@@ -178,6 +178,7 @@ export default defineComponent({
         tabindex,
         'aria-label': day.value.ariaLabel,
         'aria-hidden': !day.value.inMonth,
+        'aria-disabled': day.value.isDisabled,
       };
     });
 
